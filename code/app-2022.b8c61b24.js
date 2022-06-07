3438da2e4430a50fbfc0fd8b580bb87257187c4f
Belem Zhang <belem.zhang@intel.com>
2022-06-07 15:36:25 +0800

    init

--- code/app-2022.b8c61b24.js
+++
@@ -1,9 +0,0 @@
-import{s as f,r as v,$ as c,n as l}from"./index.fcc1d9b1.js";var u=Object.defineProperty,_=Object.getOwnPropertyDescriptor,h=(o,r,t,p)=>{for(var e=p>1?void 0:p?_(r,t):r,s=o.length-1,a;s>=0;s--)(a=o[s])&&(e=(p?a(r,t,e):a(e))||e);return p&&e&&u(r,t,e),e};let n=class extends f{static get styles(){return v`
-    `}constructor(){super()}render(){return c`
-      <app-header ?enableBack="${!0}"></app-header>
-      <div>
-        <h2>2022 中国 PWA 开发者日</h2>
-      </div>
-      <app-footer></app-footer>
-    `}};n=h([l("app-2022")],n);export{n as App2022};
-//# sourceMappingURL=app-2022.b8c61b24.js.map
