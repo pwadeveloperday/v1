b0f8241ae5378c8cd8705c80f0e90b84a60ee597
Belem Zhang <belem.zhang@intel.com>
2022-06-10 16:17:08 +0800

    ready

---
+++ code/app-about.acee1f7c.js
@@ -0,0 +1,50 @@
+import{s,r as u,$ as c,n as l}from"./index.604586b7.js";var f=Object.defineProperty,i=Object.getOwnPropertyDescriptor,m=(n,r,a,p)=>{for(var e=p>1?void 0:p?i(r,a):r,t=n.length-1,o;t>=0;t--)(o=n[t])&&(e=(p?o(r,a,e):o(e))||e);return p&&e&&f(r,a,e),e};let d=class extends s{static get styles(){return u`
+    .about {
+      padding: 0 16px;
+      margin: 2rem 0 0rem 0;
+    }
+
+    fluent-card {
+      padding: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0px;
+      box-shadow: none;
+    }
+
+    fluent-card h3 {
+      margin: 10px auto;
+    }
+
+    fluent-card p {
+      margin: 10px auto;
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+    `}constructor(){super()}render(){return c`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <h2>关于中国 PWA 开发者日</h2>
+        <fluent-card>
+          <h3>为什么要参加中国 PWA 开发者日？</h3>
+          <p>中国 PWA 开发者日的使命是帮助您使用渐进式 Web 应用（PWA）取得成功。
+          我们创建此活动是为了帮助传播有关 PWA 的技术及实践，包括如何成功构建它们以及如何将它们集成到您现有的 Web 应用中。我们不专注于推销特定的 Web 框架或技术栈。</p>
+          <p>该活动是免费的！</p>
+        </fluent-card>
+        <fluent-card>
+          <h3>谁是组织方？</h3>
+          <p>中国 PWA 开发者日是来自参与创建 PWA 技术的几家不同公司的人员之间的合作。</p>
+          <p>2021年主办方：英特尔 鼎道智联<br>
+          2022年主办方：英特尔 微软</p>
+          <p>我们的指导委员会是：</p>
+          </fluent-card>
+      </div>
+      <app-footer></app-footer>
+    `}};d=m([l("app-about")],d);export{d as AppAbout};
+//# sourceMappingURL=app-about.acee1f7c.js.map
---
+++ code/app-tutorial.4faf4320.js
@@ -0,0 +1,77 @@
+import{s as o,r as i,$ as n,n as h}from"./index.604586b7.js";var c=Object.defineProperty,w=Object.getOwnPropertyDescriptor,f=(l,r,t,C)=>{for(var e=C>1?void 0:C?w(r,t):r,a=l.length-1,p;a>=0;a--)(p=l[a])&&(e=(C?p(r,t,e):p(e))||e);return C&&e&&c(r,t,e),e};let s=class extends o{static get styles(){return i`
+    .about {
+      padding: 0 16px;
+      margin: 2rem 0px 0rem;
+    }
+
+    fluent-card {
+      border-radius: 0px;
+      box-shadow: none;
+    }
+
+    fluent-anchor svg {
+      width: 16px;
+      height: 16px;
+      margin-bottom: -2px;
+      fill: #3D3D3D;
+    }
+
+    fluent-card ul {
+      list-style: circle;
+      margin-left: -2rem;
+    }
+
+    fluent-card ul li {
+      color: red;
+      display: block;
+      list-style: circle;
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+    `}constructor(){super()}render(){return n`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <h2>PWA 教程</h2>
+        <fluent-card>
+          <ul>
+            <li>
+              <fluent-anchor href="https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/" appearance="primary">
+                30 days of PWA
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+              </fluent-anchor>
+            </li>
+            <li>
+              <fluent-anchor href="https://developer.mozilla.org/zh-CN/docs/Web/Progressive_web_apps" appearance="primary">
+                MDN: 渐进式 Web 应用（PWA）
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+              </fluent-anchor></li>
+            <li>
+              <fluent-anchor href="https://web.dev/progressive-web-apps/" appearance="primary">
+                PWA: 采用所有正确组成要素的网站
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+                </fluent-anchor></li>
+            <li>
+              <fluent-anchor href="https://www.pwabuilder.com/" appearance="primary">
+                PWA Builder
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+              </fluent-anchor>
+            </li>
+            <li>
+            <fluent-anchor href="https://www.w3.org/TR/mini-app-white-paper/comparison.html" appearance="primary">
+              W3C PWA 及小程序对比
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+            </fluent-anchor>
+          </li>
+          </ul>
+        </fluent-card>
+      </div>
+      <app-footer></app-footer>
+    `}};s=f([h("app-tutorial")],s);export{s as AppTutorial};
+//# sourceMappingURL=app-tutorial.4faf4320.js.map
