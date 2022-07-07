5a1bd70d9fff0a2b9f834ad5e2f720c5fe05975a
ibelem <106730348+pwadeveloperday@users.noreply.github.com>
2022-07-07 13:32:58 +0800

    contact picker, 2022

---
+++ code/app-coc.151c0008.js
@@ -0,0 +1,94 @@
+import{s as d,r as c,$ as l,n as m}from"./index.cde59b63.js";var h=Object.defineProperty,i=Object.getOwnPropertyDescriptor,u=(s,a,e,p)=>{for(var r=p>1?void 0:p?i(a,e):a,o=s.length-1,t;o>=0;o--)(t=s[o])&&(r=(p?t(a,e,r):t(r))||r);return p&&r&&h(a,e,r),r};let n=class extends d{static get styles(){return c`
+    .spam {
+      display: none;
+    }
+
+    .about {
+      padding: 0 16px;
+      margin: 1rem 0 0rem 0;
+      color: #3d3d3d;
+      min-height: calc(100vh - 76px);
+    }
+
+    h2 {
+      margin: 0;
+      padding: 16px 0;
+      color: rgba(0, 113, 197, 0.9);
+    }
+    
+    fluent-card {
+      padding: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0px;
+      box-shadow: none;
+      background: rgba(255, 255, 255, 0.4);
+    }
+
+    fluent-card:hover {
+      background: rgba(255, 255, 255, 0.95);
+      color: #000;
+    }
+
+    fluent-card h3 {
+      margin: 10px auto;
+    }
+
+    fluent-card p {
+      margin: 10px auto;
+    }
+
+    fluent-card svg {
+      width: 16px;
+      height: 16px;
+      margin-bottom: -2px;
+      fill: #3D3D3D;
+    }
+
+    a {
+      color: rgba(0,113,197, 0.9);
+      text-decoration: none;
+    }
+
+    a:hover {
+      color: rgba(0,113,197, 1.0);
+      border-bottom: 1px dashed rgba(0,113,197, 1);
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+    `}constructor(){super()}render(){return l`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <h2>行为准则</h2>
+        <fluent-card>
+          <h3>中国 PWA 开发者日旨在成为一个包容并欢迎所有人的活动</h3>
+          <p> 
+          中国 PWA 开发者日要求尊重每一个人，并互相尊重。每个人都有权参与并享受我们的分享，而不必担心骚扰、歧视或傲慢。</p>
+          <p>中国 PWA 开发者日不会容忍任何原因的歧视或骚扰，包括但不限于以下特征：种族、肤色、宗教（包括宗教服饰和仪容仪表）、性别、性取向、性别认同或表达、国家出身、血统、精神或身体残疾、医疗状况、怀孕、军人或退伍军人身份、遗传信息、公民身份和婚姻状况。</p>
+          <p>我们希望参加我们活动的每个人都避免发表不恰当或冒犯性的言论（诽谤、辱骂等），或从事针对他人的不恰当或冒犯的行为。</p>
+          <p>任何违反这些规则的人，包括但不限于与会者、赞助商和组织者，都可能被会议组织者开除会议。</p>
+          </fluent-card>
+        <fluent-card>
+          <h3>行为准则适用于谁？它适用于什么地方？</h3>
+          <p>行为准则适用于所有人，包括与会者、赞助商、演讲者和组织者。它适用于任何中国 PWA 开发者日主办的线上或线下活动，包括代码存储库和宣传推广渠道（微信分享、微博分享、直播平台、活动聊天等）。不容忍任何形式的不当行为或骚扰。
+          </p>
+          </fluent-card>
+          <fluent-card>
+          <h3>报告行为准则问题</h3>
+          <p>如果您需要任何形式的帮助或者报告不良情况，请联系我们：
+          </p>
+          <p>电子邮件: pwadeveloperday<span class="spam">spam</span>@<span class="spam">spam</span>outlook<span class="spam">spam</span>.com</p>
+          <p>本行为准则深受 <a href="https://pwasummit.org/code-of-conduct/">PWASummit 行为准则</a> 
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M256 64C256 46.33 270.3 32 288 32H415.1C415.1 32 415.1 32 415.1 32C420.3 32 424.5 32.86 428.2 34.43C431.1 35.98 435.5 38.27 438.6 41.3C438.6 41.35 438.6 41.4 438.7 41.44C444.9 47.66 447.1 55.78 448 63.9C448 63.94 448 63.97 448 64V192C448 209.7 433.7 224 416 224C398.3 224 384 209.7 384 192V141.3L214.6 310.6C202.1 323.1 181.9 323.1 169.4 310.6C156.9 298.1 156.9 277.9 169.4 265.4L338.7 96H288C270.3 96 256 81.67 256 64V64zM0 128C0 92.65 28.65 64 64 64H160C177.7 64 192 78.33 192 96C192 113.7 177.7 128 160 128H64V416H352V320C352 302.3 366.3 288 384 288C401.7 288 416 302.3 416 320V416C416 451.3 387.3 480 352 480H64C28.65 480 0 451.3 0 416V128z"/></svg>
+          的影响。</p>
+          </fluent-card>
+          <app-footer></app-footer>
+      </div>
+    `}};n=u([m("app-coc")],n);export{n as AppCoC};
+//# sourceMappingURL=app-coc.151c0008.js.map
---
+++ code/sample-fsa.336a2e05.js
@@ -0,0 +1,241 @@
+import{s as l,r as d,$ as p,i as g,n as m}from"./index.cde59b63.js";import"./lit-video.6fc33520.js";var b=Object.defineProperty,u=Object.getOwnPropertyDescriptor,n=(t,i,o,a)=>{for(var e=a>1?void 0:a?u(i,o):i,r=t.length-1,c;r>=0;r--)(c=t[r])&&(e=(a?c(i,o,e):c(e))||e);return a&&e&&b(i,o,e),e};let s=class extends l{constructor(){super()}async _playLocalVideo(){let t;[t]=await showOpenFilePicker();const i=await t.getFile();this.video.src=URL.createObjectURL(i)}async connectedCallback(){super.connectedCallback()}static get styles(){return d`
+    .about {
+      padding: 0 16px;
+      margin: 1rem 0 0rem 0;
+      color: #3d3d3d;
+      min-height: calc(100vh - 76px);
+    }
+
+    h2 {
+      font-size: 1.2rem;
+      color: rgba(0, 113, 197, 0.9);
+    }
+
+    fluent-card {
+      padding: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0px;
+      box-shadow: none;
+      background: rgba(255, 255, 255, 0.5);
+    }
+
+    fluent-card:hover {
+      background: rgba(255, 255, 255, 0.95);
+      color: #000;
+    }
+
+    fluent-card h3 {
+      margin: 10px auto;
+    }
+
+    fluent-card p {
+      margin: 10px auto;
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+
+    .spam {
+      display: none;
+    }
+
+    #status {
+      display: inline;
+    }
+
+    #result {
+      display: none;
+    }
+
+    .previewv {
+      margin: 16px auto;
+      text-align: left;
+      writing-mode:vertical-rl;
+      letter-spacing: 4px;
+      font-size: 1.8rem;
+    }
+
+    .previewv h3, .previewv h4 {
+      margin: 0 auto;
+    }
+
+    .previewv h4 {
+      margin-left: 16px;
+    }
+
+    a {
+      color: rgba(255, 255, 255, 0.9);
+      text-decoration: none;
+      border-bottom: 0px dashed rgba(255, 255, 255, 0.6);;
+    }
+
+    a:hover {
+      color: rgba(255, 255, 255, 1);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(255, 255, 255, 1);
+    }
+
+    #st {
+      padding: 0;
+    }
+
+    #st {
+      color: rgba(255, 255, 255, 0.9);
+    }
+
+    #st:hover {
+      color: rgba(255, 255, 255, 1.0);
+    }
+
+    .tut {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .tut a {
+      color: rgba(0, 113, 197, 0.9);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.6);
+    }
+
+    .tut a:hover {
+      color: rgba(0, 113, 197, 1);
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
+    }
+
+    .w3c {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 199, 253, 0.9);
+    }
+    .w3c:hover {
+      background-color: rgba(0, 199, 253, 1.0);
+    }
+    
+    .imp {
+      display: flex;
+      flex-direction: row;
+      gap: 16px;
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 113, 197, 0.9);
+      align-items: center;
+      justify-content: space-around;
+    }
+    .imp:hover {
+      background-color: rgba(0, 113, 197, 1.0);
+    }
+
+    .imp .yes {
+      fill: rgba(255, 255, 255, 0.9);
+    }
+
+    .imp:hover .yes {
+      fill: rgba(255, 255, 255, 1.0);
+    }
+
+    .imp .no {
+      fill: rgba(255, 255, 255, 0.1);
+    }
+
+    .imp:hover .no {
+      fill: rgba(0, 0, 0, 0.4);
+    }
+
+    .des {
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      padding: 2px 8px;
+    }
+
+    .cate, .det {
+      display: inline-block;
+    }
+    .cate .yes, .det .yes, .cate .no, .det .no{
+      display: inline-block;
+      width: 16px;
+      padding: 0 4px;
+    }
+
+    .cate {
+      border-right: 1px solid rgba(255, 255, 255, 0.2);
+      display: none;
+    }
+
+    .q {
+      margin: 16px auto;
+    }
+
+    .w3clogo {
+      margin-right: 6px;
+    }
+
+    .act {
+      text-align: center;
+    }
+
+    .act button {
+      margin-top: 16px;
+    }
+
+    `}render(){return p`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <fluent-breadcrumb>
+          <fluent-breadcrumb-item href="/">首页</fluent-breadcrumb-item>
+          <fluent-breadcrumb-item href="/sample">示例</fluent-breadcrumb-item>
+        </fluent-breadcrumb>
+        <h2>文件系统访问 (File System Access) API</h2>
+        <fluent-card class="act">
+          <lit-video 
+            id="first-video"
+            intervalreproduction="#t=1,5" 
+            option="simple" 
+            src="https://storage.googleapis.com/webfundamentals-assets/videos/chrome.mp4" 
+            type="video/mp4">
+          </lit-video>
+          <button @click="${this._playLocalVideo}">
+            播放本地视频
+          </button>
+        </fluent-card>
+        <fluent-card id="st">
+          <div class="tut">
+            <icon-webdev></icon-webdev> 
+            <a href="https://web.dev/file-system-access/" title="The File System Access API: simplifying access to local files">
+              教程：简化对本地文件的访问
+            </a>
+          </div>
+          <div class="w3c"><icon-w3c class="w3clogo"></icon-w3c> <a href="https://wicg.github.io/file-system-access/" title="File System Access">File System Access API</a></div>
+          <div class="imp">
+            <div class="des">
+              <a href="https://chromestatus.com/feature/6284708426022912" title="在 Chromium 86 版本支持">🐡 M86</a>
+            </div>
+            <div class="des">
+              <div class="det">
+              <icon-chr class="yes" title="Google Chrome 浏览器"></icon-chr>
+              <icon-edg class="yes" title="微软 Edge 浏览器"></icon-edg> <icon-ope class="yes" title="Opera 浏览器"></icon-ope> <icon-viv class="yes" title="Vivaldi 浏览器"></icon-viv>
+              <icon-saf class="no" title="Apple Safari 浏览器"></icon-saf> <icon-fir class="no" title="Mozilla Firefox 浏览器"></icon-fir>
+              </div>
+            </div>
+            <div class="des">
+              <div class="det">
+                <icon-mac class="yes" title="Mac"></icon-mac> <icon-win class="yes" title="Windows"></icon-win> <icon-lin class="yes" title="Linux"></icon-lin> 
+              </div>
+              <div class="det">
+                <icon-and class="no" title="Android"></icon-and> <icon-ios class="no" title="iOS"></icon-ios>
+              </div>
+            </div>   
+          </div>
+        </fluent-card>
+        <app-footer></app-footer>
+      </div>
+    `}};n([g("#first-video")],s.prototype,"video",2);s=n([m("sample-fsa")],s);export{s as SampleFSA};
+//# sourceMappingURL=sample-fsa.336a2e05.js.map
