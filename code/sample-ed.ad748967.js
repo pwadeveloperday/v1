6b15473f2332cff4c020649f42c5c62cf1f3f5cf
ibelem <106730348+pwadeveloperday@users.noreply.github.com>
2022-07-08 16:57:28 +0800

    fix

---
+++ code/sample-ed.ad748967.js
@@ -0,0 +1,252 @@
+import{s as l,r as d,$ as p,i as b,e as g,n as m}from"./index.2973c676.js";var u=Object.defineProperty,h=Object.getOwnPropertyDescriptor,s=(o,e,i,t)=>{for(var r=t>1?void 0:t?h(e,i):e,n=o.length-1,c;n>=0;n--)(c=o[n])&&(r=(t?c(e,i,r):c(r))||r);return t&&r&&u(e,i,r),r};let a=class extends l{constructor(){super(),this.abortController=new AbortController}async _sampleColorFromScreen(){this.abortController=new AbortController;const o=new EyeDropper;try{const e=await o.open({signal:this.abortController.signal});this._msg.innerHTML=e.sRGBHex,this._msg.setAttribute("style","background: "+e.sRGBHex)}catch(e){this._msg.innerHTML=e.message}}_abort(){this._msg.innerHTML="\u5DF2\u53D6\u6D88",this._msg.setAttribute("style","background: none"),this.abortController.abort()}async connectedCallback(){super.connectedCallback()}static get styles(){return d`
+    .about {
+      padding: 0 16px;
+      margin: 1rem 0 0rem 0;
+      color: #3d3d3d;
+      min-height: calc(100vh - 76px);
+    }
+
+    h2 {
+      font-size: 1.2rem;
+      color: rgba(0, 113, 197, 0.9);
+    }
+
+    fluent-card {
+      padding: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0px;
+      box-shadow: none;
+      background: rgba(255, 255, 255, 0.5);
+    }
+
+    fluent-card:hover {
+      background: rgba(255, 255, 255, 0.95);
+      color: #000;
+    }
+
+    fluent-card h3 {
+      margin: 10px auto;
+    }
+
+    fluent-card p {
+      margin: 10px auto;
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+
+    .spam {
+      display: none;
+    }
+
+    #status {
+      display: inline;
+    }
+
+    #result {
+      display: none;
+    }
+
+    .previewv {
+      margin: 16px auto;
+      text-align: left;
+      writing-mode:vertical-rl;
+      letter-spacing: 4px;
+      font-size: 1.8rem;
+    }
+
+    .previewv h3, .previewv h4 {
+      margin: 0 auto;
+    }
+
+    .previewv h4 {
+      margin-left: 16px;
+    }
+
+    a {
+      color: rgba(255, 255, 255, 0.9);
+
+      text-decoration: none;
+      border-bottom: 0px dashed rgba(255, 255, 255, 0.6);;
+    }
+
+    a:hover {
+      color: rgba(255, 255, 255, 1);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(255, 255, 255, 1);
+    }
+
+    #st {
+      padding: 0;
+    }
+
+    #st {
+      color: rgba(255, 255, 255, 0.9);
+    }
+
+    #st:hover {
+      color: rgba(255, 255, 255, 1.0);
+    }
+
+    .tut {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .tut a {
+      color: rgba(0, 113, 197, 0.9);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.6);
+    }
+
+    .tut a:hover {
+      color: rgba(0, 113, 197, 1);
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
+    }
+
+    .w3c {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 199, 253, 0.9);
+    }
+    .w3c:hover {
+      background-color: rgba(0, 199, 253, 1.0);
+    }
+    
+    .imp {
+      display: flex;
+      flex-direction: row;
+      gap: 16px;
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 113, 197, 0.9);
+      align-items: center;
+      justify-content: space-around;
+    }
+    .imp:hover {
+      background-color: rgba(0, 113, 197, 1.0);
+    }
+
+    .imp .yes {
+      fill: rgba(255, 255, 255, 0.9);
+    }
+
+    .imp:hover .yes {
+      fill: rgba(255, 255, 255, 1.0);
+    }
+
+    .imp .no {
+      fill: rgba(255, 255, 255, 0.1);
+    }
+
+    .imp:hover .no {
+      fill: rgba(0, 0, 0, 0.4);
+    }
+
+    .des {
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      padding: 2px 8px;
+    }
+
+    .cate, .det {
+      display: inline-block;
+    }
+    .cate .yes, .det .yes, .cate .no, .det .no{
+      display: inline-block;
+      width: 16px;
+      padding: 0 4px;
+    }
+
+    .cate {
+      border-right: 1px solid rgba(255, 255, 255, 0.2);
+      display: none;
+    }
+
+    .q {
+      margin: 16px auto;
+    }
+
+    .w3clogo {
+      margin-right: 6px;
+    }
+
+    .act div {
+      margin-top: 16px;
+    }
+
+    .act a {
+      color: rgba(0, 113, 197, 0.9);
+      cursor: pointer;
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.6);
+    }
+
+    .act a:hover {
+      color: rgba(0, 113, 197, 1);
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
+    }
+
+    #show {
+      display: none;
+    }
+
+    .act div {
+      background-color: rgba(0, 0, 0, 0.02);
+      padding: 8px 16px;
+    }
+
+    `}render(){return p`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <fluent-breadcrumb>
+          <fluent-breadcrumb-item href="/">首页</fluent-breadcrumb-item>
+          <fluent-breadcrumb-item href="/sample">示例</fluent-breadcrumb-item>
+        </fluent-breadcrumb>
+        <h2>Eyedropper API</h2>
+        <fluent-card class="act">
+          <button @click="${this._sampleColorFromScreen}">启用 Eyedropper</button>
+          <button @click="${this._abort}">中止 Eyedropper</button>
+          <div id="msg"></div>
+        </fluent-card>
+        <fluent-card id="st">
+          <div class="tut">
+            <icon-webdev></icon-webdev> 
+            <a href="https://web.dev/eyedropper/" title="Picking colors of any pixel on the screen with the EyeDropper API ">
+              教程：选择屏幕上任意像素的颜色
+            </a>
+          </div>
+          <div class="w3c"><icon-w3c class="w3clogo"></icon-w3c> <a href="https://wicg.github.io/eyedropper-api/" title="EyeDropper API">EyeDropper API</a></div>
+          <div class="imp">
+            <div class="des">
+              <a href="https://chromestatus.com/feature/6304275594477568" title="在 Chromium 95 版本支持">🐡 M95</a>
+            </div>
+            <div class="des">
+              <div class="det">
+              <icon-chr class="yes" title="Google Chrome 浏览器"></icon-chr>
+              <icon-edg class="yes" title="微软 Edge 浏览器"></icon-edg> <icon-ope class="yes" title="Opera 浏览器"></icon-ope> <icon-viv class="yes" title="Vivaldi 浏览器"></icon-viv>
+              <icon-saf class="no" title="Apple Safari 浏览器"></icon-saf> <icon-fir class="no" title="Mozilla Firefox 浏览器"></icon-fir>
+              </div>
+            </div>
+            <div class="des">
+              <div class="det">
+                <icon-mac class="yes" title="Mac"></icon-mac> <icon-win class="yes" title="Windows"></icon-win> <icon-lin class="yes" title="Linux"></icon-lin> 
+              </div>
+              <div class="det">
+                <icon-and class="no" title="Android"></icon-and> <icon-ios class="no" title="iOS"></icon-ios>
+              </div>
+            </div>   
+          </div>
+        </fluent-card>
+        <app-footer></app-footer>
+      </div>
+    `}};s([b("#msg")],a.prototype,"_msg",2);s([g({type:Object})],a.prototype,"abortController",2);a=s([m("sample-ed")],a);export{a as SampleED};
+//# sourceMappingURL=sample-ed.ad748967.js.map
