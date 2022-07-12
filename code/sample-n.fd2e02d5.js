c3d41d779a8cfeb7925cd0ceddc7fbdb734be5b4
ibelem <106730348+pwadeveloperday@users.noreply.github.com>
2022-07-12 11:07:56 +0800

    data footer

---
+++ code/sample-n.fd2e02d5.js
@@ -0,0 +1,245 @@
+import{s,r as d,$ as l,n as u}from"./index.f0ddad4c.js";var p=Object.defineProperty,g=Object.getOwnPropertyDescriptor,b=(o,t,n,r)=>{for(var e=r>1?void 0:r?g(t,n):t,a=o.length-1,i;a>=0;a--)(i=o[a])&&(e=(r?i(t,n,e):i(e))||e);return r&&e&&p(t,n,e),e};let c=class extends s{_randomIntFromInterval(o,t){return Math.floor(Math.random()*(t-o+1)+o)}_n(){let o=this._randomIntFromInterval(1,999),t=`\u7B2C ${o.toString()} \u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5`;const e={body:`\u4E3A\u52A0\u901F\u63A8\u52A8\u6E10\u8FDB\u5F0F Web \u5E94\u7528 (PWA) \u5728\u4E2D\u56FD\u7684\u53D1\u5C55\uFF0C\u5FAE\u8F6F\u4E0E\u82F1\u7279\u5C14\u643A\u624B\u4E3E\u529E\u201C\u7B2C ${o.toString()} \u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5\u201D\u3002`,icon:"assets/icons/icon_96.png"};Notification.requestPermission(function(a){console.log("\u7528\u6237\u9009\u62E9 ",a),a!=="granted"?console.log("\u672A\u83B7\u5F97\u901A\u77E5\u6743\u9650!"):navigator.serviceWorker.ready.then(function(i){i.showNotification(t,e)})})}async connectedCallback(){super.connectedCallback()}static get styles(){return d`
+    .about {
+      padding: 0 16px;
+      margin: 1rem 0 0rem 0;
+      color: #3d3d3d;
+      min-height: calc(100vh - 76px);
+    }
+
+    h2 {
+      font-size: 1.2rem;
+      color: rgba(0, 113, 197, 0.9);
+    }
+
+    fluent-card {
+      padding: 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0px;
+      box-shadow: none;
+      background: rgba(255, 255, 255, 0.5);
+    }
+
+    fluent-card:hover {
+      background: rgba(255, 255, 255, 0.95);
+      color: #000;
+    }
+
+    fluent-card h3 {
+      margin: 10px auto;
+    }
+
+    fluent-card p {
+      margin: 10px auto;
+    }
+
+    @media(prefers-color-scheme: light) {
+    }
+
+    @media(prefers-color-scheme: dark) {
+    }
+
+    @media (min-width: 1024px) {
+    }
+
+    .spam {
+      display: none;
+    }
+
+    #status {
+      display: inline;
+    }
+
+    #result {
+      display: none;
+    }
+
+    .previewv {
+      margin: 16px auto;
+      text-align: left;
+      writing-mode:vertical-rl;
+      letter-spacing: 4px;
+      font-size: 1.8rem;
+    }
+
+    .previewv h3, .previewv h4 {
+      margin: 0 auto;
+    }
+
+    .previewv h4 {
+      margin-left: 16px;
+    }
+
+    a {
+      color: rgba(255, 255, 255, 0.9);
+
+      text-decoration: none;
+      border-bottom: 0px dashed rgba(255, 255, 255, 0.6);;
+    }
+
+    a:hover {
+      color: rgba(255, 255, 255, 1);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(255, 255, 255, 1);
+    }
+
+    #st {
+      padding: 0;
+    }
+
+    #st {
+      color: rgba(255, 255, 255, 0.9);
+    }
+
+    #st:hover {
+      color: rgba(255, 255, 255, 1.0);
+    }
+
+    .tut {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .tut a {
+      color: rgba(0, 113, 197, 0.9);
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.6);
+    }
+
+    .tut a:hover {
+      color: rgba(0, 113, 197, 1);
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
+    }
+
+    .w3c {
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 199, 253, 0.9);
+    }
+    .w3c:hover {
+      background-color: rgba(0, 199, 253, 1.0);
+    }
+    
+    .imp {
+      display: flex;
+      flex-direction: row;
+      gap: 16px;
+      padding: 16px;
+      margin: 0 auto;
+      text-align: center;
+      background-color: rgba(0, 113, 197, 0.9);
+      align-items: center;
+      justify-content: space-around;
+    }
+    .imp:hover {
+      background-color: rgba(0, 113, 197, 1.0);
+    }
+
+    .imp .yes {
+      fill: rgba(255, 255, 255, 0.9);
+    }
+
+    .imp:hover .yes {
+      fill: rgba(255, 255, 255, 1.0);
+    }
+
+    .imp .no {
+      fill: rgba(255, 255, 255, 0.1);
+    }
+
+    .imp:hover .no {
+      fill: rgba(0, 0, 0, 0.4);
+    }
+
+    .des {
+      border: 1px solid rgba(255, 255, 255, 0.2);
+      padding: 2px 8px;
+    }
+
+    .cate, .det {
+      display: inline-block;
+    }
+    .cate .yes, .det .yes, .cate .no, .det .no{
+      display: inline-block;
+      width: 16px;
+      padding: 0 4px;
+    }
+
+    .cate {
+      border-right: 1px solid rgba(255, 255, 255, 0.2);
+      display: none;
+    }
+
+    .q {
+      margin: 16px auto;
+    }
+
+    .w3clogo {
+      margin-right: 6px;
+    }
+
+    .act div {
+      margin-top: 16px;
+    }
+
+    .act a {
+      color: rgba(0, 113, 197, 0.9);
+      cursor: pointer;
+      text-decoration: none;
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.6);
+    }
+
+    .act a:hover {
+      color: rgba(0, 113, 197, 1);
+      border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
+    }
+
+    #show {
+      display: none;
+    }
+
+    `}constructor(){super()}render(){return l`
+      <app-header ?enableBack="${!0}"></app-header>
+      <div class="about">
+        <fluent-breadcrumb>
+          <fluent-breadcrumb-item href="/">首页</fluent-breadcrumb-item>
+          <fluent-breadcrumb-item href="/sample">示例</fluent-breadcrumb-item>
+        </fluent-breadcrumb>
+        <h2>通知 (Notification) API</h2>
+        <fluent-card class="act">
+          <button @click="${this._n}">显示通知</button>
+        </fluent-card>
+        <fluent-card id="st">
+          <div class="tut">
+            <icon-webdev></icon-webdev> 
+            <a href="https://web.dev/notifications/" title="Web Push and Notifications">
+              教程：网络推送和通知
+            </a>
+          </div>
+          <div class="w3c">whatwg <a href="https://ications.spec.whatwg.org/" title="Notifications API">Notifications API</a></div>
+          <div class="imp">
+            <div class="des">
+              <a href="https://chromestatus.com/feature/5064350557536256" title="在 Chromium 102 版本支持">🌐 M20</a>
+            </div>
+            <div class="des">
+              <div class="det">
+              <icon-chr class="yes" title="Google Chrome 浏览器"></icon-chr>
+              <icon-edg class="yes" title="微软 Edge 浏览器"></icon-edg> <icon-ope class="yes" title="Opera 浏览器"></icon-ope> <icon-viv class="yes" title="Vivaldi 浏览器"></icon-viv>
+              <icon-saf class="yes" title="Apple Safari 浏览器"></icon-saf> <icon-fir class="yes" title="Mozilla Firefox 浏览器"></icon-fir>
+              </div>
+            </div>
+            <div class="des">
+              <div class="det">
+                <icon-mac class="yes" title="Mac"></icon-mac> <icon-win class="yes" title="Windows"></icon-win> <icon-lin class="yes" title="Linux"></icon-lin> 
+              </div>
+              <div class="det">
+                <icon-and class="yes" title="Android"></icon-and> <icon-ios class="yes" title="iOS"></icon-ios>
+              </div>
+            </div>   
+          </div>
+        </fluent-card>
+        <app-footer></app-footer>
+      </div>
+    `}};c=b([u("sample-n")],c);export{c as SampleN};
+//# sourceMappingURL=sample-n.fd2e02d5.js.map
