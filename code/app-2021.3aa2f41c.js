9469e60f2cb7c58ca1efa98387e7ca316971f885
Belem Zhang <belem.zhang@intel.com>
2022-08-04 16:46:56 +0800

    Updates

--- code/app-2021.3aa2f41c.js
+++ code/app-2021.3aa2f41c.js
@@ -1,4 +1,4 @@
-import{a as l,s as c,r as h,$ as p,o as C,e as d,n as g}from"./index.82190d99.js";/**
+import{a as l,s as c,r as h,$ as p,o as C,e as d,n as g}from"./index.3b178049.js";/**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
@@ -153,4 +153,4 @@ import{a as l,s as c,r as h,$ as p,o as C,e as d,n as g}from"./index.82190d99.js
           <app-footer-home></app-footer-home>
         </div>
       `}else return p`<div>No data</div>`}};o([d({type:String})],s.prototype,"description",2);o([d({type:Object})],s.prototype,"jsondata",2);o([d({type:String})],s.prototype,"screenwidth",2);o([d({type:String})],s.prototype,"screenheight",2);o([d({type:String})],s.prototype,"topic",2);o([f(".ids")],s.prototype,"ids",2);s=o([g("app-2021")],s);export{s as App2021};
-//# sourceMappingURL=app-2021.36442631.js.map
+//# sourceMappingURL=app-2021.3aa2f41c.js.map
--- code/app-blog.93a923ed.js
+++ code/app-blog.93a923ed.js
@@ -1,4 +1,4 @@
-import{s as p,r as c,$ as d,o as n,e as F,n as g}from"./index.82190d99.js";var E=Object.defineProperty,b=Object.getOwnPropertyDescriptor,s=(e,o,u,t)=>{for(var a=t>1?void 0:t?b(o,u):o,i=e.length-1,r;i>=0;i--)(r=e[i])&&(a=(t?r(o,u,a):r(a))||a);return t&&a&&E(o,u,a),a};let l=class extends p{constructor(){super(),this.jsondata={name:"blog",blog:[{id:1,path:"china-pwa-developer-day-2022",title:"\u7B2C\u4E8C\u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5",des:"\u7B2C\u4E8C\u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5 (China PWA Developer Day 2022) \u5373\u5C06\u4E8E 2022 \u5E74 8 \u6708 6 \u65E5\u7EBF\u4E0A\u4E3E\u884C\u3002\u6B22\u8FCE\u60A8\u7684\u53C2\u4E0E\uFF01",date:"2022-08-06",authors:[{author:"\u5B8B\u9752\u89C1 (Edwin Song)",pos:"\u9996\u5E2D\u4EA7\u54C1\u7ECF\u7406",com:"\u5FAE\u8F6F Edge \xB7 \u5F00\u53D1\u8005\u751F\u6001 Bio",iconid:"edwin",icon:"assets/2022/people/120/edwin.png",icon5:"assets/2022/people/500/edwin.png",bio:"\u4ECE\u4E1A 20 \u4F59\u5E74\uFF0C\u5386\u7ECF\u8F6F\u4EF6\u5F00\u53D1\u5230\u4EA7\u54C1\u7ECF\u7406\uFF0C\u524D\u7AEF\u5E94\u7528\u5230\u4E91\u8BA1\u7B97\u4E4B\u95F4\u7684\u98CE\u4E91\u53D8\u5E7B\u3002\u66FE\u5728\u82F1\u7279\u5C14\u5DE5\u4F5C\u4E86 14 \u5E74\uFF0C\u4E3B\u8981\u4ECE\u4E8B\u4E0E\u6027\u80FD\u4F18\u5316\u3001GPU \u9A71\u52A8\u7A0B\u5E8F\u548C Chromium \u5185\u6838\u4F18\u5316\u76F8\u5173\u7684\u8F6F\u4EF6\u5F00\u53D1\u548C\u56E2\u961F\u7BA1\u7406\u3002\u4E4B\u540E\uFF0C\u53D7 Chromium \u65B0\u67B6\u6784/\u521B\u65B0\u7684\u542F\u53D1\uFF0C\u6709\u4E00\u6BB5\u77ED\u6682\u7684 H5 WebOS \u521B\u4E1A\u7ECF\u5386\uFF0C2016 \u5E74\u52A0\u5165\u5FAE\u8F6F\u4E91\u8BA1\u7B97\u56E2\u961F\uFF0C\u5728 Azure RDS for MySQL \u548C\u533A\u5757\u94FE PaaS \u670D\u52A1\u4E0A\u5DE5\u4F5C\u4E864\u5E74\u591A\uFF0C\u4F5C\u4E3A Chormium \u5FE0\u7C89\uFF0C\u5728\u5FAE\u8F6F Edge \u6D4F\u89C8\u5668\u5168\u9762\u91C7\u7528 Chromium \u5185\u6838\u540E\uFF0C\u52A0\u5165 Edge PWA \u56E2\u961F\uFF0C\u4E3A\u6784\u5EFA WebOS \u7684\u68A6\u60F3\u518D\u51FA\u53D1\uFF01"},{author:"\u5F20\u654F (Belem Zhang)",pos:"\u8F6F\u4EF6\u6280\u672F\u7ECF\u7406",com:"\u82F1\u7279\u5C14 SATG Web \u5E73\u53F0\u5DE5\u7A0B",iconid:"belem",icon:"assets/2022/people/120/belem.png",icon5:"assets/2022/people/500/belem.png",bio:"\u5F20\u654F\uFF0C\u53C2\u4E0E\u4E86 Crosswork\u3001<a href='https://webnn.dev/' target='_blank'>Web Neural Network (WebNN)</a>\u3001\u6E10\u8FDB\u5F0F Web \u5E94\u7528 \uFF08PWA\uFF09\u7B49 Web \u9879\u76EE\u3002 \u5728 2013 \u5E74\u4E4B\u524D\uFF0C\u4ED6\u662F Opera Software \u7684\u8F6F\u4EF6\u5DE5\u7A0B\u7ECF\u7406\u5E76\u9886\u5BFC\u4E86\u5BA2\u6237\u7AEF\u8FD0\u8425\u548C\u5173\u952E\u6570\u636E\u4EA7\u54C1\u56E2\u961F\u3002 \u5F20\u654F\u5728\u6D4F\u89C8\u5668\u4EA7\u54C1\u53CA\u8D28\u91CF\u3001\u7F51\u7AD9\u517C\u5BB9\u6027\u3001Web API \u53CA\u5E94\u7528\u7B49\u65B9\u9762\u6709\u591A\u5E74\u7ECF\u9A8C\uFF0C\u5F20\u654F\u6B63\u548C\u56E2\u961F\u52AA\u529B\u5C06\u82F1\u7279\u5C14 XPU \u7684\u5F3A\u5927\u529F\u80FD\u5F15\u5165\u5230 Web\uFF0C\u5E76\u4E0E Web \u793E\u533A\u643A\u624B\u63A8\u52A8 PWA \u6280\u672F\u5728\u56FD\u5185\u7684\u53D1\u5C55\u548C\u843D\u5730\u3002"}]}]}}static get styles(){return c`
+import{s as c,r as F,$ as n,o as s,e as g,n as b}from"./index.3b178049.js";var E=Object.defineProperty,h=Object.getOwnPropertyDescriptor,p=(e,r,u,a)=>{for(var t=a>1?void 0:a?h(r,u):r,o=e.length-1,i;o>=0;o--)(i=e[o])&&(t=(a?i(r,u,t):i(t))||t);return a&&t&&E(r,u,t),t};let d=class extends c{constructor(){super(),this.jsondata={name:"blog",blog:[{id:1,path:"china-pwa-developer-day-2022",title:"\u7B2C\u4E8C\u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5",des:"\u7B2C\u4E8C\u5C4A\u4E2D\u56FD PWA \u5F00\u53D1\u8005\u65E5 (China PWA Developer Day 2022) \u5373\u5C06\u4E8E 2022 \u5E74 8 \u6708 6 \u65E5\u7EBF\u4E0A\u4E3E\u884C\u3002\u6B22\u8FCE\u60A8\u7684\u53C2\u4E0E\uFF01",des2:"",date:"2022-08-06",authors:[{author:"\u5B8B\u9752\u89C1 (Edwin Song)",pos:"\u9996\u5E2D\u4EA7\u54C1\u7ECF\u7406",com:"\u5FAE\u8F6F Edge \xB7 \u5F00\u53D1\u8005\u751F\u6001 Bio",iconid:"edwin",icon:"assets/2022/people/120/edwin.png",icon5:"assets/2022/people/500/edwin.png",bio:"\u4ECE\u4E1A 20 \u4F59\u5E74\uFF0C\u5386\u7ECF\u8F6F\u4EF6\u5F00\u53D1\u5230\u4EA7\u54C1\u7ECF\u7406\uFF0C\u524D\u7AEF\u5E94\u7528\u5230\u4E91\u8BA1\u7B97\u4E4B\u95F4\u7684\u98CE\u4E91\u53D8\u5E7B\u3002\u66FE\u5728\u82F1\u7279\u5C14\u5DE5\u4F5C\u4E86 14 \u5E74\uFF0C\u4E3B\u8981\u4ECE\u4E8B\u4E0E\u6027\u80FD\u4F18\u5316\u3001GPU \u9A71\u52A8\u7A0B\u5E8F\u548C Chromium \u5185\u6838\u4F18\u5316\u76F8\u5173\u7684\u8F6F\u4EF6\u5F00\u53D1\u548C\u56E2\u961F\u7BA1\u7406\u3002\u4E4B\u540E\uFF0C\u53D7 Chromium \u65B0\u67B6\u6784/\u521B\u65B0\u7684\u542F\u53D1\uFF0C\u6709\u4E00\u6BB5\u77ED\u6682\u7684 H5 WebOS \u521B\u4E1A\u7ECF\u5386\uFF0C2016 \u5E74\u52A0\u5165\u5FAE\u8F6F\u4E91\u8BA1\u7B97\u56E2\u961F\uFF0C\u5728 Azure RDS for MySQL \u548C\u533A\u5757\u94FE PaaS \u670D\u52A1\u4E0A\u5DE5\u4F5C\u4E864\u5E74\u591A\uFF0C\u4F5C\u4E3A Chormium \u5FE0\u7C89\uFF0C\u5728\u5FAE\u8F6F Edge \u6D4F\u89C8\u5668\u5168\u9762\u91C7\u7528 Chromium \u5185\u6838\u540E\uFF0C\u52A0\u5165 Edge PWA \u56E2\u961F\uFF0C\u4E3A\u6784\u5EFA WebOS \u7684\u68A6\u60F3\u518D\u51FA\u53D1\uFF01"},{author:"\u5F20\u654F (Belem Zhang)",pos:"\u8F6F\u4EF6\u6280\u672F\u7ECF\u7406",com:"\u82F1\u7279\u5C14 SATG Web \u5E73\u53F0\u5DE5\u7A0B",iconid:"belem",icon:"assets/2022/people/120/belem.png",icon5:"assets/2022/people/500/belem.png",bio:"\u5F20\u654F\uFF0C\u53C2\u4E0E\u4E86 Crosswork\u3001<a href='https://webnn.dev/' target='_blank'>Web Neural Network (WebNN)</a>\u3001\u6E10\u8FDB\u5F0F Web \u5E94\u7528 \uFF08PWA\uFF09\u7B49 Web \u9879\u76EE\u3002 \u5728 2013 \u5E74\u4E4B\u524D\uFF0C\u4ED6\u662F Opera Software \u7684\u8F6F\u4EF6\u5DE5\u7A0B\u7ECF\u7406\u5E76\u9886\u5BFC\u4E86\u5BA2\u6237\u7AEF\u8FD0\u8425\u548C\u5173\u952E\u6570\u636E\u4EA7\u54C1\u56E2\u961F\u3002 \u5F20\u654F\u5728\u6D4F\u89C8\u5668\u4EA7\u54C1\u53CA\u8D28\u91CF\u3001\u7F51\u7AD9\u517C\u5BB9\u6027\u3001Web API \u53CA\u5E94\u7528\u7B49\u65B9\u9762\u6709\u591A\u5E74\u7ECF\u9A8C\uFF0C\u5F20\u654F\u6B63\u548C\u56E2\u961F\u52AA\u529B\u5C06\u82F1\u7279\u5C14 XPU \u7684\u5F3A\u5927\u529F\u80FD\u5F15\u5165\u5230 Web\uFF0C\u5E76\u4E0E Web \u793E\u533A\u643A\u624B\u63A8\u52A8 PWA \u6280\u672F\u5728\u56FD\u5185\u7684\u53D1\u5C55\u548C\u843D\u5730\u3002"}]}]}}static get styles(){return F`
     .about {
       padding: 0 16px;
       margin: 2rem 0px 0rem;
@@ -182,6 +182,14 @@ import{s as p,r as c,$ as d,o as n,e as F,n as g}from"./index.82190d99.js";var E
       background-image: url('assets/2022/people/120/edwin.png');
     }
 
+    #icon_wpe {
+      background-image: url('assets/2022/people/120/wpe.png');
+    }
+
+    #icon_hongjuan {
+      background-image: url('assets/2022/people/120/hongjuan.png');
+    }
+
     a {
       color: rgba(0, 113, 197, 0.9);
       cursor: pointer;
@@ -200,13 +208,13 @@ import{s as p,r as c,$ as d,o as n,e as F,n as g}from"./index.82190d99.js";var E
       border-bottom: 1px dashed rgba(0, 113, 197, 0.9);
     }
 
-    `}_getpath(){let e=location.pathname.replace("/blog/","");return e=e.replace("/blog",""),console.log(e.trim()),e.trim()}async fetchData(){const e=await fetch("/assets/data/blog.json");this.jsondata=await e.json()}async connectedCallback(){super.connectedCallback(),await this._getpath(),await this.fetchData()}render(){if(this.jsondata){let e="";if(this._getpath()){for(let u of this.jsondata.blog)if(this._getpath()===u.path){let t=u.des.replace(/\n/g,"<br>"),a="";for(let r of u.authors)a+=`
+    `}_getpath(){let e=location.pathname.replace("/blog/","");return e=e.replace("/blog",""),console.log(e.trim()),e.trim()}async fetchData(){const e=await fetch("/assets/data/blog.json");this.jsondata=await e.json()}async connectedCallback(){super.connectedCallback(),await this._getpath(),await this.fetchData()}render(){if(this.jsondata){let e="";if(this._getpath()){for(let u of this.jsondata.blog)if(this._getpath()===u.path){let a=u.des.replace(/\n/g,"<br>"),t=u.des2.replace(/\n/g,"<br>"),o="";for(let l of u.authors)o+=`
                 <div class="dessub">
-                  <div class="avatar" id="icon_${r.iconid}"></div>
+                  <div class="avatar" id="icon_${l.iconid}"></div>
                   <div class="description">
-                    <div class="nametitle">${r.author}</div>
-                    <div class="team">${r.pos}</div>
-                    <div class="team">${r.com}</div>
+                    <div class="nametitle">${l.author}</div>
+                    <div class="team">${l.pos}</div>
+                    <div class="team">${l.com}</div>
                   </div>
                 </div>
               `;e+=`
@@ -214,43 +222,44 @@ import{s as p,r as c,$ as d,o as n,e as F,n as g}from"./index.82190d99.js";var E
                 <div class="title">${u.title}</div>
                 <div class="date">${u.date}</div>
                 <div class="article">
+                  ${a}
                   ${t}
                 </div>
                 <div class="details">
-                  ${a}
+                  ${o}
                 </div>
               </fluent-card>
-            `}}else for(let u of this.jsondata.blog){let t=u.des.replace(/\n/g,"<br>");t=t.split("<!-- summary -->")[0],e+=`
+            `}}else for(let u of this.jsondata.blog){let a=u.des.replace(/\n/g,"<br>");a=a.split("<!-- summary -->")[0],e+=`
             <fluent-card id="id_${u.id}" class="ids">
               <div class="title">
                 <a href="/blog/${u.path}">${u.title}</a>
               </div>
               <div class="article">
-                ${t}
+                ${a}
               </div>
             </fluent-card>
-          `}let o="";return this._getpath()||(o=`
+          `}let r="";return this._getpath()||(r=`
           <h2><a href="/blog" title="\u535A\u5BA2">Blog</a></h2>
-          <div class="desc">\u4E3A\u60A8\u51C6\u5907\u7684\u5173\u4E8E PWA \u53CA Fugu API \u7684\u66F4\u65B0\u548C\u6545\u4E8B</div>
-        `),d`
+          <div class="desc">\u4E3A\u60A8\u51C6\u5907\u7684\u5173\u4E8E Web, PWA \u53CA Fugu API \u7684\u66F4\u65B0\u548C\u6545\u4E8B</div>
+        `),n`
         <app-header ?enableBack="${!0}"></app-header>
         <div class="about">
           <fluent-breadcrumb>
           <fluent-breadcrumb-item href="/">首页</fluent-breadcrumb-item>
           <fluent-breadcrumb-item href="/blog">Blog</fluent-breadcrumb-item>
           </fluent-breadcrumb>
-            ${n(o)}
+            ${s(r)}
           <div id="schedule">
-            ${n(e)}
+            ${s(e)}
           </div>
         </div>
         <app-footer></app-footer>
-      `}else return d`<app-header ?enableBack="${!0}"></app-header>
+      `}else return n`<app-header ?enableBack="${!0}"></app-header>
       <div class="about">
         <h2>Blog</h2>
         <div class="desc">为您准备的关于 PWA 及 Fugu API 的更新和故事</div>
         <fluent-card>
         </fluent-card>
       </div>
-      <app-footer></app-footer>`}};s([F({type:Object})],l.prototype,"jsondata",2);l=s([g("app-blog")],l);export{l as AppBlog};
-//# sourceMappingURL=app-blog.d3492d3b.js.map
+      <app-footer></app-footer>`}};p([g({type:Object})],d.prototype,"jsondata",2);d=p([b("app-blog")],d);export{d as AppBlog};
+//# sourceMappingURL=app-blog.93a923ed.js.map
